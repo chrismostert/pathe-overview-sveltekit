a2ed143a6aa990654dc6ae4d3104e4ddb30024e1
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-05-03 20:17:29 +0200

    Color times

--- client/src/InfoBlock.svelte
+++ client/src/InfoBlock.svelte
@@ -1,22 +1,21 @@
 <script>
-    export let movie;
+  import Time from "./Time.svelte";
+  export let movie;
 </script>
 
 <div class="flex flex-col">
-    <!-- Description -->
-    <p>{movie.description}</p>
+  <!-- Description -->
+  <p>{movie.description}</p>
 
-    <!-- Date/times -->
-    <div class="mt-4">
-      {#each movie.dateTimes as datetime}
-        <p class="font-medium">{datetime.day}</p>
-        <div class="flex flex-wrap">
-          {#each datetime.times as time}
-            <p class="mr-4">
-              {time.start} - {time.end} ({time.label})
-            </p>
-          {/each}
-        </div>
-      {/each}
-    </div>
+  <!-- Date/times -->
+  <div class="mt-4">
+    {#each movie.dateTimes as datetime}
+      <p class="font-medium">{datetime.day}</p>
+      <div class="flex flex-wrap">
+        {#each datetime.times as time}
+          <Time {time} />
+        {/each}
+      </div>
+    {/each}
   </div>
+</div>
---
+++ client/src/Time.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let time;
+    function getColor(label) {
+        if (label == "3D") {
+            return "bg-[#FFC426]";
+        }
+        if (
+            label == "Music" ||
+            label == "50PLUS" ||
+            label == "Arthouse" ||
+            label == "DOCS" ||
+            label == "Voorpremière" ||
+            label == "Opera" ||
+            label == "Horror Night"
+        ) {
+            return "text-white bg-black";
+        }
+        if (label == "Expat Night") {
+            return "bg-[#B09A61]";
+        }
+        if (label == "Ladies Night") {
+            return "text-white bg-[#c821ac]";
+        }
+        return "bg-[#CFD6EA]";
+    }
+</script>
+
+<div class={`inline-block rounded-md mr-4 mb-1 p-1 ${getColor(time.label)}`}>
+    {time.start} – {time.end} ({time.label})
+</div>
