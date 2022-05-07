e66fd858a30ce0f4a9b4699b9a12377006aa888d
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-05-07 16:39:01 +0200

    Only show text once popcorn image is loaded

--- client/src/MovieContainer/Spinner.svelte
+++ client/src/MovieContainer/Spinner.svelte
@@ -2,13 +2,22 @@
   import { onDestroy } from "svelte";
 
   let show = false;
+  let imageLoaded = false;
+  
   const timeout = setTimeout(() => (show = true), 200);
   onDestroy(() => clearTimeout(timeout));
 </script>
 
 {#if show}
   <div class="text-center">
-    <img src="popcorn.gif" alt="loader" class="mx-auto" />
-    <p>Loading movies...</p>
+    <img
+      src="popcorn.gif"
+      alt="loader"
+      class="mx-auto"
+      on:load={() => imageLoaded = true}
+    />
+    {#if imageLoaded}
+      <div>Loading movies...</div>
+    {/if}
   </div>
 {/if}
