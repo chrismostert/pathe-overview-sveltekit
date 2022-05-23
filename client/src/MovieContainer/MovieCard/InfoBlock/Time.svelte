08bb4e8b340c1947b1ad1938cdb637aea20d62ca
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-05-23 17:20:00 +0200

    Simplify times color

--- client/src/MovieContainer/MovieCard/InfoBlock/Time.svelte
+++ client/src/MovieContainer/MovieCard/InfoBlock/Time.svelte
@@ -4,30 +4,17 @@
         if (label == "3D") {
             return "bg-[#FFC426]";
         }
-        if (
-            label == "Music" ||
-            label == "50PLUS" ||
-            label == "Arthouse" ||
-            label == "DOCS" ||
-            label == "Voorpremière" ||
-            label == "Opera" ||
-            label == "Horror Night"
-        ) {
-            return "text-white bg-[#188FA7]";
-        }
-        if (label == "Expat Night") {
-            return "text-white bg-[#5C5346]";
-        }
-        if (label == "Ladies Night") {
-            return "bg-[#F49FBC]";
-        }
         if (label == "2D") {
             return "bg-[#D8DBE2]";
         }
-        return ""
+        return "text-white bg-[#188FA7]";
     }
 </script>
 
-<div class={`inline-block rounded-md mr-1 mb-2 px-1.5 text-sm text-center ${getColor(time.label)}`}>
+<div
+    class={`inline-block rounded-md mr-1 mb-2 px-1.5 text-sm text-center ${getColor(
+        time.label
+    )}`}
+>
     {time.start} – {time.end} ({time.label})
 </div>
