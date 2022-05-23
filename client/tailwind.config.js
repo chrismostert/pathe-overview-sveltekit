0e10e8374299aae1eaf6020f7624422965633127
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-05-23 19:15:18 +0200

    Add clear button styling

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,7 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ['./public/index.html', './src/**/*.svelte'],
   theme: {
     extend: {},
+    screens: {
+      'xs': '490px',
+      ...defaultTheme.screens,
+    },
   },
   plugins: [],
 }
