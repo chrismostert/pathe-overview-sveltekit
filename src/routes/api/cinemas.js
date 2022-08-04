66e352be0c761dcb94114e77cb65db1de5d93ff1
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-04 15:37:17 +0200

    Add cinemas, rating and movie routes to API

---
+++ src/routes/api/cinemas.js
@@ -0,0 +1,33 @@
+import { load } from 'cheerio';
+
+export async function GET() {
+	try {
+		let page = await fetch('https://www.pathe.nl/bioscoopagenda');
+		let cont = await page.text();
+		let $ = load(cont);
+
+		let res = new Set();
+		$('.filter__input-list li').each((_, elem) => {
+			let cinema_elem = $(elem).find('.cinema.checkbox');
+			res.add(
+				JSON.stringify({
+					cinema_name: cinema_elem.attr('data-show-value'),
+					cinema_id: cinema_elem.attr('value')
+				})
+			);
+		});
+
+		return {
+			body: {
+				cinemas: [...res].map(JSON.parse)
+			}
+		};
+	} catch (_) {
+		return {
+			status: 500,
+			body: {
+				cinemas: []
+			}
+		};
+	}
+}
