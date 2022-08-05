c2631555f96b95af27c6f7f925a9cb35767eb645
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-05 12:28:38 +0200

    Formatting

--- src/routes/cinema/[cinema_id].svelte
+++ src/routes/cinema/[cinema_id].svelte
@@ -1,15 +1,15 @@
 <script context="module">
-    export async function load( {params } ) {
-        return {
-            props: {
-                cinema_id: params.cinema_id
-            }
-        }
-    }
+	export async function load({ params }) {
+		return {
+			props: {
+				cinema_id: params.cinema_id
+			}
+		};
+	}
 </script>
 
 <script>
-    export let cinema_id;
+	export let cinema_id;
 </script>
 
 <h1>Hello {cinema_id}</h1>
