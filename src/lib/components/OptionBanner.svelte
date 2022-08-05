a6fbdaf788eeb454f9478f47f2f5fc3f413d9246
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-05 12:26:35 +0200

    Add cinema selection with routing

---
+++ src/lib/components/OptionBanner.svelte
@@ -0,0 +1,25 @@
+<script>
+	import { goto } from '$app/navigation';
+	export let cinemas;
+
+	let selected = '0';
+</script>
+
+<div class="py-2 px-4 shadow-md mb-1">
+	<div class="flex flex-row flex-wrap justify-between -mb-2">
+		<div class="mb-2 mr-2">
+			Bioscoop:
+			<select
+				id="cinemas"
+				bind:value={selected}
+				on:change={goto(`/cinema/${selected}`)}
+				class="outline-none border-2 bg-white"
+			>
+				<option value="0" disabled selected>Selecteer een bioscoop</option>
+				{#each cinemas as cinema}
+					<option value={cinema.cinema_id}>{cinema.cinema_name}</option>
+				{/each}
+			</select>
+		</div>
+	</div>
+</div>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,32 @@
-<script>
-	import '../app.css';
+<script context="module">
+	export async function load({ fetch }) {
+		let response = await fetch('/api/cinemas');
+
+		if (response.ok) {
+			return {
+				props: {
+					cinemas: (await response.json()).cinemas
+				}
+			};
+		}
+
+		return {
+			props: {
+				cinemas: []
+			}
+		};
+	}
 </script>
 
-<slot />
+<script>
+	import '../app.css';
+	import OptionBanner from '$lib/components/OptionBanner.svelte';
+	export let cinemas;
+</script>
+
+<div class="max-w-4xl mx-auto">
+	<OptionBanner {cinemas} />
+	<main>
+		<slot />
+	</main>
+</div>
