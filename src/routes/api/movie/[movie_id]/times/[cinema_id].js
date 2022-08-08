877e297e9c393799a8ead079a09434f71a9e55b3
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-08 11:48:04 +0200

    Add times API functionality

---
+++ src/routes/api/movie/[movie_id]/times/[cinema_id].js
@@ -0,0 +1,31 @@
+import { load } from 'cheerio';
+
+export async function GET({ params }) {
+	let page = await fetch(
+		`https://www.pathe.nl/film/${params.movie_id}/agenda?cinemas=${params.cinema_id}`
+	);
+	let cont = await page.text();
+	let $ = load(cont);
+
+	const date_times = $('.schedule__section')
+		.map((_, day) => {
+			return {
+				day: $(day).find('.schedule__day').text(),
+				times: $(day)
+					.find('.schedule-time__content')
+					.map((_, time) => {
+						return {
+							start: $(time).find('.schedule-time__start').text(),
+							end: $(time).find('.schedule-time__end').text(),
+							label: $(time).find('.schedule-time__label').text().trim()
+						};
+					})
+					.toArray()
+			};
+		})
+		.toArray();
+
+	return {
+		body: date_times
+	};
+}
