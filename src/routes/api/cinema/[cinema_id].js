66764cfbd6b81027162c64712929625e2154e6c6
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-08 22:56:06 +0200

    Add times

--- src/routes/api/cinema/[cinema_id].js
+++ src/routes/api/cinema/[cinema_id].js
@@ -1,19 +1,30 @@
 import pLimit from 'p-limit';
-const limit = pLimit(40);
+const limit = pLimit(30);
 
 export async function GET({ params, url }) {
 	let movielist = await fetch(`${url.origin}/api/movie`).then(
 		async (response) => await response.json()
 	)
 
-	let movies = await Promise.all(
+	let movies = Promise.all(
 		movielist.movies.map((movie) =>
 			limit(() => fetch(`${url.origin}/api/movie/${movie.id}`).then((res) => res.json()))
 		)
 	)
 
+	let times = Promise.all(
+		movielist.movies.map((movie) =>
+			limit(() => fetch(`${url.origin}/api/movie/${movie.id}/times/${params.cinema_id}`).then((res) => res.json()))
+		)
+	)
 
 	return {
-		body: movies
+		headers: {
+			'Cache-Control': 'max-age=3600, public'
+		},
+		body: {
+			movies: await movies,
+			times: await times
+		}
 	};
 }
