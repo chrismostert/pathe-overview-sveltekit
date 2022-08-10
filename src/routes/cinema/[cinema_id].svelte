aa04efc5ad605219347f4bb49618a93d6c12f133
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-08-10 11:10:30 +0200

    cache test

--- src/routes/cinema/[cinema_id].svelte
+++ src/routes/cinema/[cinema_id].svelte
@@ -1,3 +1,21 @@
+<script context="module">
+	export async function load({params, fetch, url}) {
+		const resp = await fetch(`${url.origin}/api/cinema/${params.cinema_id}`);
+		const resp_obj = await resp.json();
+
+		return {
+			props: {
+				movies: resp_obj.movies,
+				times: resp_obj.times
+			},
+			cache: {
+				"maxage": 3600,
+				"private": false
+			}
+		}
+	}
+</script>
+
 <script>
 	import MovieContainer from '$lib/components/MovieContainer.svelte';
 	export let movies, times;
