0dcb8d00b24c355f0392415f6966632562cd7501
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-11-01 13:35:07 +0100

    Break up front page layout and cinema page layout

---
+++ src/routes/cinema/[cinema_id]/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Banner from '$lib/components/Banner.svelte';
+	import { ypos } from '$lib/store.js';
+	export let data;
+	let y;
+
+	$: $ypos = y;
+</script>
+
+<svelte:window bind:scrollY={y} />
+
+<Banner cinemas={data.cinemas} />
+<slot />
