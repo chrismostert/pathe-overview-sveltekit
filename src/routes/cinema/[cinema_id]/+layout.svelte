5203007c7202226cf43d2557a93db5f481bdb525
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2022-11-16 19:05:35 +0100

    Add error message in cinema page layout

--- src/routes/cinema/[cinema_id]/+layout.svelte
+++ src/routes/cinema/[cinema_id]/+layout.svelte
@@ -2,6 +2,8 @@
 	import Banner from '$lib/components/Banner.svelte';
 	import { ypos, window_width } from '$lib/store.js';
 	export let data;
+
+	const FALLBACK_NAME = 'Onjuiste bioscoop!';
 	let selected_cinema;
 
 	$: selected_cinema = data.cinemas.filter((elem) => elem.cinema_id == data.selected_cinema)[0]
@@ -10,5 +12,11 @@
 
 <svelte:window bind:scrollY={$ypos} bind:innerWidth={$window_width} />
 
-<Banner cinemas={data.cinemas} {selected_cinema} />
+<Banner cinemas={data.cinemas} selected_cinema={selected_cinema || FALLBACK_NAME} />
+{#if !selected_cinema}
+	<div class="mt-4 text-xl">
+		De bioscoop die je opgevraagd hebt lijkt niet te bestaan. Kies een andere in het hamburgermenu
+		hierboven.
+	</div>
+{/if}
 <slot />
