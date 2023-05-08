011753905165b476922e256dd9a307f7f1ee8b2f
Chris Mostert <15890652+chrismostert@users.noreply.github.com>
2023-05-08 19:27:08 +0200

    Simplify icon display

--- src/lib/components/Ratings.svelte
+++ src/lib/components/Ratings.svelte
@@ -1,13 +1,5 @@
 <script>
 	export let rt;
-
-	// Replace external URL with a local version of the icon since the RT API
-	// seems to give back a non-existent URL
-	const regex = /\/rt-icons\/(.+)\.png/
-	function get_local_icon_url(url, kind) {
-		return `/${kind}-${url.match(regex)[1]}.svg`;
-	}
-
 </script>
 
 {#if rt.vanity}
@@ -16,7 +8,7 @@
 			<div class="grid grid-cols-2 items-center justify-items-center">
 				<div class="flex items-center">
 					{#if rt?.scores?.criticsIconUrl}
-						<img src={get_local_icon_url(rt.scores.criticsIconUrl, 'critics')} alt="rt-icon" class="mr-1 h-10 w-10" />
+						<img src={`/critics-${rt.scores.criticsScore < 60 ? 'rotten' : 'fresh'}.svg`} alt="rt-icon" class="mr-1 h-10 w-10" />
 						<p>{rt.scores.criticsScore}%</p>
 					{:else}
 						<img src="/critics-undefined.svg" alt="rt-icon" class="mr-1 h-10 w-10" />
@@ -25,7 +17,7 @@
 				</div>
 				<div class="flex items-center">
 					{#if rt?.scores?.audienceIconUrl}
-						<img src={get_local_icon_url(rt.scores.audienceIconUrl, 'audience')} alt="rt-icon" class="mr-1 h-10 w-10" />
+						<img src={`/audience-${rt.scores.audienceScore < 60 ? 'green_popcorn' : 'red_popcorn'}.svg`} alt="rt-icon" class="mr-1 h-10 w-10" />
 						<p>{rt.scores.audienceScore}%</p>
 					{:else}
 						<img src="/audience-undefined.svg" alt="rt-icon" class="mr-1 h-10 w-10" />
